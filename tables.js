c272fec9d01ff6e0e7ceeee4fa0951a9ea5bedfa
edsaav <edsaav@gmail.com>
2018-06-12 13:57:18 -0700

    Adding tables.js

---
+++ tables.js
@@ -0,0 +1,79 @@
+function generateTable(args){
+  console.log('Inside generateTable function')
+  tableData = args.tableData;
+  exportData = args.exportData || args.tableData;
+  enableExport = args.enableExport || false;
+  filename = args.filename || args.title.replace(/\s/g, '');
+  cols = [];
+  for (i = 0; i < args.cols.length; i++) {
+    cols.push({ 'data': args.cols[i] })
+  };
+  console.log(cols)
+  title = args.title;
+  tableId = '#' + args.tableId || '#customTable';
+  
+  $(document).ready(function() {
+    $(tableId).DataTable({
+      'data': tableData,
+      'columns': cols,
+      'pageLength': 25,
+      dom: `<"${tableId}-header"f>tpi`,
+      'pagingType': 'simple',
+      'info': true
+    });
+  });
+
+  $(`${tableId}-header`).ready(function() {
+    console.log(`${tableId}-header is ready`)
+    $(`${tableId}-header`).prepend(`<div class='chart-header' style='float:left;margin: 12px 0 10px 0;'>${title}</div>`);
+    if (enableExport) {
+      console.log('Export option is on')
+      $(`${tableId}-header`).prepend(`<button id='${tableId.replace('#','')}-export' class='exportButton'>Export</button>`);
+      $(`${tableId}-export`).click(function(){
+        console.log('Click!')
+        downloadCSV({ filename: `${filename}.csv`, data: exportData });
+      });
+    }   
+  });
+}
+
+function arrayToCSV(args) {
+  var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+  data = args.data || null;
+  if (data == null || !data.length) {
+    return null;
+  }
+  columnDelimiter = args.columnDelimiter || ',';
+  lineDelimiter = args.lineDelimiter || '\n';
+  keys = Object.keys(data[0]);
+  result = '';
+  result += keys.join(columnDelimiter);
+  result += lineDelimiter;
+  data.forEach(function(item) {
+    ctr = 0;
+    keys.forEach(function(key) {
+      if (ctr > 0) result += columnDelimiter;
+      result += item[key];
+      ctr++;
+    });
+    result += lineDelimiter;
+  });
+  return result;
+}
+
+function downloadCSV(args) {
+  var data, filename, link;
+  var csv = arrayToCSV({
+    data: args.data
+  });
+  if (csv == null) return;
+  filename = args.filename || 'export.csv';
+  if (!csv.match(/^data:text\/csv/i)) {
+    csv = 'data:text/csv;charset=utf-8,' + csv;
+  }
+  data = encodeURI(csv);
+  link = document.createElement('a');
+  link.setAttribute('href', data);
+  link.setAttribute('download', filename);
+  link.click();
+}
