2747c5a030d12c30c842dd5bf162bc26f552bee2
edsaav <edsaav@gmail.com>
2018-06-13 08:36:43 -0700

    Improve date sorting through moment.js

--- tables.js
+++ tables.js
@@ -11,6 +11,7 @@ function generateTable(args){
   tableId = '#' + args.tableId || '#customTable';
   
   $(document).ready(function() {
+    $.fn.dataTable.moment( 'MMM D, YYYY' )
     $(tableId).DataTable({
       'data': tableData,
       'columns': cols,
@@ -82,3 +83,19 @@ function generateExportButton(args){
     $(whereId).append(btn);
   });
 }
+
+$.fn.dataTable.moment = function ( format, locale ) {
+    var types = $.fn.dataTable.ext.type;
+ 
+    // Add type detection
+    types.detect.unshift( function ( d ) {
+        return moment( d, format, locale, true ).isValid() ?
+            'moment-'+format :
+            null;
+    } );
+ 
+    // Add sorting method - use an integer for the sorting
+    types.order[ 'moment-'+format+'-pre' ] = function ( d ) {
+        return moment( d, format, locale, true ).unix();
+    };
+};
