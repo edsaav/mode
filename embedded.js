64b75b83736ccf04f6af5d83124634ec8c13c71b
edsaav <edsaav@gmail.com>
2018-07-27 10:25:54 -0700

    Initial embedded report functions

---
+++ embedded.js
@@ -0,0 +1,61 @@
+function setupReport(tableArgs, replaceTextArgs) {
+  generateTable(tableArgs);
+
+  $(document).ready(function() {
+    replaceText(replaceTextArgs);
+  });
+}
+
+function generateTable(args) {
+  let tableData = args.tableData;
+  let cols = [];
+  for (i = 0; i < args.cols.length; i++) {
+    cols.push({
+      'data': args.cols[i]
+    });
+  };
+  let title = args.title;
+  let tableId = '#' + args.tableId || '#customTable';
+
+  $(document).ready(function() {
+    $(tableId).DataTable({
+      'data': tableData,
+      'columns': cols,
+      'pageLength': 25,
+      dom: `<"${tableId}-header"f>tpi`,
+      'pagingType': 'simple',
+      'info': true,
+      'order': []
+    });
+  });
+
+  addHeaders(args.cols, tableId);
+
+  $(`${tableId}-header`).ready(function() {
+    $(`${tableId}-header`).prepend(`<div class='chart-header' style='float:left;margin: 12px 0 10px 0;'>${title}</div>`);
+  });
+}
+
+function addHeaders(columns, tableId) {
+  for (let i = 0; i < columns.length; i++) {
+    $(`${tableId} thead tr`).append("<th>" + columns[i].split('_').join(' ') + "</th>");
+  };
+}
+
+function replaceText(args) {
+  let emptyBigNumber = args.emptyBigNumber || '--';
+  let chartRenderError = args.chartRenderError || 'Sorry, no data for the selected time period';
+  let tableRenderError = args.tableRenderError || 'Sorry, no data for the selected time period';
+  let textSpansToReplace = args.textSpansToReplace;
+
+  for (let i = 0; i < textSpansToReplace.length; i++) {
+    $(`text tspan:contains(${textSpansToReplace[i][0]})`).replaceWith(textSpansToReplace[i][1]);
+  }
+
+  $("span.fb-content:contains('{{ctrl.content}}')").text(emptyBigNumber);
+  $("h3:contains('Sorry, unable to render a chart with the given data')").text(chartRenderError);
+  $(".message:contains('No rows returned')").replaceWith(tableRenderError).css('margin', '15px !important');
+  $("td:contains('alert')").each(function() {
+    $(this).text('broadcaster');
+  });
+}
